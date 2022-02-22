00f5ac9eb2f790fe5fc998cd3b3ac91128930945
vitormenezess <vitormenezes43@icloud.com>
2022-02-21 23:05:13 -0300

    html/style

---
+++ calculadora/scripts.js
@@ -0,0 +1,32 @@
+const numberButtons = document.querySelectorAll("[data-number]")
+const operationButtons = document.querySelectorAll("[data-operator]")
+const equalsButtons = document.querySelector("[data-equals]")
+const deleteButtons = document.querySelector("[data-delete]")
+const allClearButton = document.querySelector("[data-all-clear]")
+const previousOperandTextElement = document.querySelector("[data-previous-operand]")
+const currentOperandTextElement = document.querySelector("[data-current-operand]")
+
+class Calculator{
+    constructor(previousOperandTextElement,currentOperandTextElement){
+        this.previousOperandTextElement = previousOperandTextElement
+        this.currentOperandTextElement = currentOperandTextElement
+    }
+    clear(){
+        this.currentOperand = ''
+        this.previousOperand = ''
+        this.operation = undefined
+
+    }
+    updateDisplay(){
+        this.previousOperandTextElement.innerText = this.previousOperand
+        this.currentOperandTextElement.innerText = this.currentOperand
+    }
+}
+
+const calculator = new Calculator(previousOperandTextElement,currentOperandTextElement)
+
+allClearButton.addEventListener("click", () =>{
+    calculator.clear()
+    calculator.updateDisplay()
+
+})
